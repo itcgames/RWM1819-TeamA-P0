7e557378ba62800756408260b6fe988218cef594
JackDalton3110 <c00205943@itcarlow.ie>
2018-09-25 13:00:21 +0100

    initial commit

---
+++ Test/p0TeamA.js
@@ -0,0 +1,20 @@
+/* global p0TeamA, describe, it, expect, should */
+
+describe('p0TeamA()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(p0TeamA).to.be.a('function');
+
+  });
+
+  it('return modulo 2', function () {
+    expect(modulo_first(2)).to.equal(2);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
---
+++ src/p0TeamA.js
@@ -0,0 +1,8 @@
+/*! p0TeamA v0.0.0 - MIT license */
+'use strict';
+
+var p0TeamA = function () {
+  // your code goes here
+
+  return modulo_first(2)
+}
